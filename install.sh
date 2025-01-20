4c8cbf2fe1a5673ee0b5426ff516f2a2b43452ca
daniandriyani <42364371+daniandriyan@users.noreply.github.com>
2025-01-20 11:04:29 +0700

    Update and rename instal.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 pkg update
+pkg upgrade
 pkg install x11-repo
 pkg install termux-x11-nightly
+pkg install tur-repo
 pkg install pulseaudio
-pkg update
 pkg install proot-distro
+pkg install wget
+pkg install git
 pd login debian
 apt update -y
 apt install sudo nano adduser -y
